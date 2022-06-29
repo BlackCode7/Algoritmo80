b32e723e7b11b8f6187556fb184cba4214e06134
Anderson <BlackCode7@outlook.com>
2022-06-29 06:58:42 -0300

    remove comments of the code

--- src/Algoritmo83/Algoritmo83.java
+++ src/Algoritmo83/Algoritmo83.java
@@ -3,20 +3,7 @@ package Algoritmo83;
 import java.util.Scanner;
 
 public class Algoritmo83 {
-	/*  real numero, inverso, absoluto;
-		imprima "digite numero:";
-		leia numero;
-		se( numero > O. )
-		{ inverso <- 1 I numero;
-		imprima "\ninverso: "
-		inverso;}
-		senao
-		{ absoluto <- numero * -1;
-		# ou absoluto <- abs(numero);
-		imprima "\nabsoluto: "
-		absoluto;}
-		imprima "\n"; */
-		
+			
 	public static void main(String[] args) {
 
 		double numero = 10;
