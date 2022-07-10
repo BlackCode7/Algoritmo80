6e6698626586f592485dd2a67423b10e5b7192d3
Anderson <BlackCode7@outlook.com>
2022-07-10 11:51:21 -0300

    condicionais aninhados if / else if / else

--- src/Algoritmo85_IfsAninhados/Algoritmo85IfsAninhados.java
+++ src/Algoritmo85_IfsAninhados/Algoritmo85IfsAninhados.java
@@ -1,10 +1,35 @@
 package Algoritmo85_IfsAninhados;
 
 public class Algoritmo85IfsAninhados {
+	
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		double a = 12, b = 54, c = 90, max = 0;
+		
+		if ( a > b) {
+			if( a > c) {
+				max = a;
+				System.out.println("max recebe a : " + a);
+			}
+			else {
+				max = c;
+				System.out.println("max recebe c : " + c);
+			}
+		} else {
+			if( b > c) {
+				max = b;
+				System.out.println("else - max recebe b : " + b);
+			}else {
+				max = c;
+				System.out.println("else - max recebe c : " + c);
+			}
+		}
 	}
+	
+	
+	
+	
 
 }
